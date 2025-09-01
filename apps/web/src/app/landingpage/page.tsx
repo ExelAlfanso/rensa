d946a1aa41cd890ff3289c4ecc12f7c48ba2cf59
Alfredrown <alfredoradhinalm@gmail.com>
2025-09-01 18:29:02 +0700

    feat: add landing page component with styled text

---
+++ apps/web/src/app/landingpage/page.tsx
@@ -0,0 +1,14 @@
+export default function LandingPage() {
+    return (
+        <div className="relative flex justify-center items-center h-96 m-40">
+            <div className="absolute text-5xl text-center font-bold mix-blend-color-burn text-[#56AD3B]">
+                Where Every Picture<br />
+                Tells Its Recipe
+            </div>
+            <div className="absolute text-5xl text-center font-bold mix-blend-exclusion text-white ">
+                Where Every Picture<br />
+                Tells Its Recipe
+            </div>
+        </div>
+    );
+}
