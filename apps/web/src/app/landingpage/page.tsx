45cb7c827761566169c8d913782dceb6574e025b
Alfredrown <alfredoradhinalm@gmail.com>
2025-09-02 11:10:11 +0700

    feat: add Unsplash API integration and update dependencies

--- apps/web/src/app/landingpage/page.tsx
+++ apps/web/src/app/landingpage/page.tsx
@@ -1,14 +1,31 @@
+"use client";
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function LandingPage() {
-    return (
-        <div className="relative flex justify-center items-center h-96 m-40">
-            <div className="absolute text-5xl text-center font-bold mix-blend-color-burn text-[#56AD3B]">
-                Where Every Picture<br />
-                Tells Its Recipe
-            </div>
-            <div className="absolute text-5xl text-center font-bold mix-blend-exclusion text-white ">
-                Where Every Picture<br />
-                Tells Its Recipe
-            </div>
+  return (
+    <div>
+      {/* <div className="-z-100">
+        <div id="container-1">
+          <img
+            src="https://source.unsplash.com/random/"
+            alt="Random"
+            className="w-404 h-504"
+          />
         </div>
-    );
+      </div> */}
+      <div className="relative flex justify-center items-center h-96 m-40">
+        <div className="absolute text-5xl text-center font-bold mix-blend-color-burn text-[#56AD3B]">
+          Where Every Picture
+          <br />
+          Tells Its Recipe
+        </div>
+        <div className="absolute text-5xl text-center font-bold mix-blend-exclusion text-white ">
+          Where Every Picture
+          <br />
+          Tells Its Recipe
+        </div>
+      </div>
+    </div>
+  );
 }
