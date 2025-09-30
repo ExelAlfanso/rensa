3227073be6da7bc80c20c2d2b9e8c85890004835
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-09-30 13:56:44 +0700

    fix: home responsiveness

---
+++ apps/web/src/app/datas/homeDatas.tsx
@@ -0,0 +1,60 @@
+//TODO: Update responsiveness of text and images
+
+export const heroImagesData = [
+  {
+    id: 1,
+    src: "/images/hero/hero1.JPG",
+    className:
+      "-top-[5vh] left-[0vw] lg:top-[12vh] lg:left-[8vw] w-[35vw] lg:w-[28vw] h-[36vh]",
+  },
+  {
+    id: 2,
+    src: "/images/hero/hero2.JPG",
+    className:
+      "bottom-[15vh] -left-[10vw] lg:bottom-[5vh] lg:left-[16vw] w-[55vw] lg:w-[16vw] h-[25vh] lg:h-[40vh]",
+  },
+  {
+    id: 3,
+    src: "/images/hero/hero3.JPG",
+    className:
+      "top-[18vh] right-[0vw] lg:top-[10vh] lg:right-[10vw] w-[40vw] lg:w-[32vw] h-[20vh] lg:h-[40vh]",
+  },
+  {
+    id: 4,
+    src: "/images/hero/hero4.JPG",
+    className:
+      "lg:bottom-[6vh] lg:right-[16vw] bottom-[0vh] right-[3vw] w-[23vw] h-[30vh] lg:h-[32vh]",
+  },
+];
+
+// datas/cards.ts
+export const cardData = [
+  {
+    id: 1,
+    src: "/images/image10.JPG",
+    position:
+      "absolute top-[8vh] left-[7vw] lg:top-[4vh] lg:left-[8vw] w-[30vw] lg:w-[28vw] h-[18vh] lg:h-[36vh]",
+    motion: "smoothCard1Y",
+  },
+  {
+    id: 2,
+    src: "/images/image11.JPG",
+    position:
+      "absolute top-[72vh] left-[8vw] lg:left-[12vw] w-[20vw] h-[25vh] lg:h-[35vh]",
+    motion: "smoothCard2Y",
+  },
+  {
+    id: 3,
+    src: "/images/image12.JPG",
+    position:
+      "absolute top-[42vh] lg:top-[32vh] right-[14vw] w-[32vw] h-[25vh] lg:h-[40vh]",
+    motion: "smoothCard3Y",
+  },
+  {
+    id: 4,
+    src: "/images/image13.JPG",
+    position:
+      "absolute top-[92vh] right-[6vw] w-[40vw] lg:w-[30vw] h-[30vh] lg:h-[36vh]",
+    motion: "smoothCard4Y",
+  },
+];
