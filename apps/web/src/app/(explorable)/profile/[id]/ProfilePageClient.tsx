a6d0f0a904ced1a9c8e3a533d2ba416ad6157ae6
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-12-30 06:57:45 +0700

    fix: responsiveness on photo page roll grid

--- apps/web/src/app/(explorable)/profile/[id]/ProfilePageClient.tsx
+++ apps/web/src/app/(explorable)/profile/[id]/ProfilePageClient.tsx
@@ -82,7 +82,7 @@ export default function ProfilePageClient({
         onRollDelete={handleRollDelete}
         onRollUpdate={handleRollUpdate}
       >
-        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white px-68">
+        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white">
           <div className="w-[131px] h-[131px] relative rounded-full overflow-hidden">
             <Image
               src={profileData.user?.avatar || "/profile.jpg"}
@@ -91,7 +91,7 @@ export default function ProfilePageClient({
               className="object-cover w-full h-full"
             />
           </div>
-          <Heading size="l" className="text-black mt-4">
+          <Heading size="l" className="mt-4 text-black">
             @{profileData.user?.username || "User Name"}
           </Heading>
           <div className="flex flex-row items-center justify-center gap-4 mt-4">
@@ -99,7 +99,7 @@ export default function ProfilePageClient({
 
             {isOwner && <AccentButton>Edit Profile</AccentButton>}
           </div>
-          <div className="flex flex-col items-start justify-center gap-6 mt-10 xl:mt-0 w-full px-6">
+          <div className="flex flex-col items-start justify-center w-full gap-6 mt-10 xl:mt-0 px-30">
             <ProfileRollFilterDropdown setFilter={setFilter} />
             <RollList rolls={rolls} isOwner={isOwner} />
           </div>
