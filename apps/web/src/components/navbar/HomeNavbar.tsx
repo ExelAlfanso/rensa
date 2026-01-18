2fbbdc871f4dec407b6c1b9421e9ec8b3ccd9f18
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2026-01-18 11:03:33 +0700

    refactor: adjust className for Heading component in HomeNavbar

--- apps/web/src/components/navbar/HomeNavbar.tsx
+++ apps/web/src/components/navbar/HomeNavbar.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React from "react";
 import Logo from "../icons/Logo";
 import Heading from "../Heading";
 import LinkTertiaryButton from "../buttons/LinkButtons/LinkTertiaryButton";
@@ -7,13 +6,14 @@ import LinkPrimaryButton from "../buttons/LinkButtons/LinkPrimaryButton";
 import Link from "next/link";
 
 const HomeNavbar = () => {
-  // const user = useAuthStore((state) => state.user);
   return (
     <nav className="z-30 left-1/2 top-5 -translate-x-1/2 fixed w-[90%] lg:w-[70%] xl:w-[80%] h-14 md:h-18 text-black rounded-[48px] flex items-center justify-between bg-white-200 shadow-lg">
       <div className="flex flex-row items-center gap-1 lg:gap-4 ml-2 lg:ml-6">
         <Logo size={"s"}></Logo>
         <Link href="/home">
-          <Heading size="s">Rensa</Heading>
+          <Heading size="s" className="hidden lg:flex">
+            Rensa
+          </Heading>
         </Link>
       </div>
       <div className="flex flex-row items-center justify-center gap-6 mr-6">
